c37f8a186eaa274fd3e6a6898c5cb38d687eada8
Stefan Cvetkovic <stefanlex90@gmail.com>
2020-02-04 15:34:54 +0100

    Added question-answer fixtures importer

--- application/cli/fixture_importer.py
+++ application/cli/fixture_importer.py
@@ -1,11 +1,13 @@
 from application import db
-from application.models import User
+from application.models import User, Question, Answer
 
 
 def import_fixture(fixture):
     fixture_model = fixture['model']
     if fixture_model == 'User':
         import_user_fixture(fixture)
+    if fixture_model == 'Question':
+        import_question_fixture(fixture)
 
 
 def import_user_fixture(fixture):
@@ -14,5 +16,17 @@ def import_user_fixture(fixture):
                           last_name=user['last_name'])
         user_model.set_password(user['password'])
         db.session.add(user_model)
+
+    db.session.commit()
+
+
+def import_question_fixture(fixture):
+    for question in fixture['data']:
+        question_model = Question(question_title=question['question_title'])
+        db.session.add(question_model)
+        for answer in question['answers']:
+            answer_model = Answer(answer_title=answer['answer_title'], correct=answer['correct'],
+                                  question=question_model)
+            db.session.add(answer_model)
 
     db.session.commit()
